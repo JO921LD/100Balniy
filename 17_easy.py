47daad503763e9f9778c1e36339b82b5bc998e0b
JO921LD <89468298+JO921LD@users.noreply.github.com>
2024-07-17 23:11:17 +0300

    Add files via upload

---
+++ 17_easy.py
@@ -0,0 +1,12 @@
+from math import floor
+f = open("17_easy.txt")
+data = [int(i) for i in f.readlines()]
+mmax, count = 0, 0
+f.close()
+for i in range(1, len(data) - 2, 2):
+    Generation = data[i] + floor(abs(data[i] - data[i - 1]) ** 0.5)
+    Delta = abs(data[i + 1] - Generation)
+    mmax = max(mmax, Delta)
+    if Generation / data[i + 1] >= 0.95:
+        count += 1
+print(mmax, count)
