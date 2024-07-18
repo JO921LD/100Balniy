2900f395b3da512e0c3a5a58e181cdb2bd1344d0
JO921LD <89468298+JO921LD@users.noreply.github.com>
2024-07-18 23:53:39 +0300

    Update 17_easy.py

--- 17_easy.py
+++ 17_easy.py
@@ -1,12 +1,11 @@
-from math import floor
 f = open("17_easy.txt")
 data = [int(i) for i in f.readlines()]
-mmax, count = 0, 0
 f.close()
-for i in range(1, len(data) - 2, 2):
-    Generation = data[i] + floor(abs(data[i] - data[i - 1]) ** 0.5)
-    Delta = abs(data[i + 1] - Generation)
-    mmax = max(mmax, Delta)
-    if Generation / data[i + 1] >= 0.95:
+count, mmin = 0, 99999999
+for i in range(len(data) - 5):
+    srznach = sum([data[i + j] for j in range(5)]) / 5
+    exp = data[i] - data[i + 4] + data[i + 3] + data[i + 2] - data[i + 1]
+    if srznach < exp:
+        mmin = min(mmin, exp)
         count += 1
-print(mmax, count)
+print(count, mmin)
